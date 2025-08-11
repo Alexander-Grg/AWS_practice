c76204ecd2728c6e89a4f73572558df91b7ebba9
Alexander Grigoryev <a.a.grigoryev@icloud.com>
2025-08-11 14:17:40 +0700

    Few edits

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -1,8 +1,9 @@
 import './HomeFeedPage.css';
 import React from "react";
 
-import DesktopNavigation  from '../components/DesktopNavigation';
 import { Auth } from 'aws-amplify';
+
+import DesktopNavigation  from '../components/DesktopNavigation';
 import DesktopSidebar     from '../components/DesktopSidebar';
 import ActivityFeed from '../components/ActivityFeed';
 import ActivityForm from '../components/ActivityForm';
@@ -23,7 +24,7 @@ export default function HomeFeedPage() {
     try {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/home`
       const res = await fetch(backend_url, {
-          headers: {
+        headers: {
           Authorization: `Bearer ${localStorage.getItem("access_token")}`
         },
         method: "GET"
@@ -41,6 +42,9 @@ export default function HomeFeedPage() {
 
   const checkAuth = async () => {
     Auth.currentAuthenticatedUser({
+      // Optional, By default is false. 
+      // If set to true, this call will send a 
+      // request to Cognito to get the latest user data
       bypassCache: false 
     })
     .then((user) => {
@@ -54,7 +58,7 @@ export default function HomeFeedPage() {
     })
     .catch((err) => console.log(err));
   };
-
+  
   React.useEffect(()=>{
     //prevents double call
     if (dataFetchedRef.current) return;
