0c153650feb4ffe8ac0a71d51c9762545d511c40
Alexander <a.a.grigoryev@icloud.com>
2025-12-05 03:39:31 +0000

    Added initial Ansible configuration, fixed variables for Terraform, added new instance to AWS. Dev container was alrso rebuilt

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -6,3 +6,13 @@ resource "aws_instance" "app_server" {
     Name = "AWS_PRACTICE"
   }
 }
+
+output "instance_public_ip" {
+  value = aws_instance.app_server.public_ip
+  description = "Public IP for SSH access"
+}
+
+output "instance_public_dns" {
+  value = aws_instance.app_server.public_dns
+  description = "Public DNS for SSH access"
+}
