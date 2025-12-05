38e91918bf22d1bfb9ffd6ebbf9b4322198240dd
Alexander Grigoryev <a.a.grigoryev@icloud.com>
2025-12-05 11:22:14 +0700

    Configured cloud and local environments, fixed the variables for the frontend, created a script launcher for the docker compose

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -18,7 +18,7 @@ export default function HomeFeedPage() {
 
   const loadData = async () => {
     try {
-      const backend_url = `/api/activities/home`
+      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/home`
       const res = await fetch(backend_url, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("access_token")}`
--- frontend-react-js/src/pages/UserFeedPage.js
+++ frontend-react-js/src/pages/UserFeedPage.js
@@ -19,7 +19,7 @@ export default function UserFeedPage() {
 
   const loadData = async () => {
     try {
-      const backend_url = `/api/activities/${title}`
+      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/${title}`
       const res = await fetch(backend_url, {
         method: "GET"
       });
