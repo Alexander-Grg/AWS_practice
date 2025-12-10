a941b6da578ed2291ff58d15b33ee97c569e880f
Alexander Grigoryev <a.a.grigoryev@icloud.com>
2025-12-10 14:16:04 +0700

    - Added ssh only SG to AWS VPC - Added SSH pub key to all hosts - Ansible configured for hosts - Created and executed 3 playbooks: basic server configuration with all the packages and updates; mobitoring tools are added plus log rotation; webservers intalled;

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.subnet_2a.id
+  key_name      = aws_key_pair.ansible_key.key_name
  
   vpc_security_group_ids = [aws_security_group.ssh_only.id]
 
