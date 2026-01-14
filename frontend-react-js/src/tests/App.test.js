7307fd976856b68fcec9417b8dcea76d1aebf7e0
Alexander Grigoryev <a.a.grigoryev@icloud.com>
2026-01-14 13:11:54 +0700

    files moved wip frontend tests

--- frontend-react-js/src/tests/App.test.js
+++ frontend-react-js/src/tests/App.test.js
@@ -1,5 +1,7 @@
+// frontend-react-js/src/tests/App.test.js
+
 import { render, screen } from '@testing-library/react';
-import App from '../src/App';
+import App from '../App'; // Now it is just one level up
 
 test('renders without crashing', () => {
   render(<App />);
